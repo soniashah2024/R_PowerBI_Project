b4baf0b281195be2fa12b994d5ccf2c0611e4411
Sonia Shah <156097446+soniashah2024@users.noreply.github.com>
2024-02-27 22:48:17 +0000

    Add files via upload

---
+++ R_PowerBI_Assignment.R
@@ -0,0 +1,50 @@
+#Step 1
+
+df<- read.csv("https://public.tableau.com/app/sample-data/HollywoodsMostProfitableStories.csv")
+
+#Take a look at the data: 
+
+View(df)
+
+#Load library:
+
+install.packages("tidyverse")
+
+#Import library
+
+library(tidyverse)
+
+# Check data types:
+
+str(df)
+
+# Check for missing values:
+
+colSums(is.na(df))
+
+#Drop missing values
+
+df<-na.omit(df) df<-df %>% drop_na()
+
+
+
+df <- na.omit(df) or df <- df %>% drop_na()
+
+# check to make sure that the rows have been removed
+
+colSums(is.na(df))
+
+
+#Summary Statistics:
+summary(df)
+
+
+#scatterplot
+ggplot(df, aes(x=Lead.Studio, y=Rotten.Tomatoes..)) + geom_point()+ scale_y_continuous(labels = scales::comma)+coord_cartesian(ylim = c(0, 110))+theme(axis.text.x = element_text(angle = 90))
+
+#bar chart
+ggplot(df, aes(x=Year)) + geom_bar()
+
+#Export clean data
+write.csv(df, "C:\Temp\clean_df.csv")
+
